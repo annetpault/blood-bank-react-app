bef2652d572b1b0fa13d3da8933483f7159328d4
annetpault <annetmca25@gmail.com>
2026-07-13 10:39:16 +0530

    Home page completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AddDonor from './components/AddDonor'
+import ViewBar from './components/ViewBar'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <AddDonor />
+    
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home />} />
+<Route path='/add' element={<AddDonor />} />
+<Route path='/view' element={<ViewBar />} />
+</Routes>
+</BrowserRouter>
     </>
   )
 }
